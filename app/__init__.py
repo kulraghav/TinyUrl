0f83d899d82ac4a016d6d3c426270f7b3db09113
Paul Ventura <christianpaulventura@gmail.com>
2020-04-14 13:49:52 +0800

    initialize database

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
+    db.init_app(app)
+
+    @app.before_first_request
+    def init_database():
+        db.create_all()
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
