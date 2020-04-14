d2a8c3231a9e4bf8683fb5fa010297d0ff150129
Paul Ventura <christianpaulventura@gmail.com>
2020-04-14 13:42:20 +0800

    move the create_app method to init file

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+
