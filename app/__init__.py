6b913328ee9eadcf584c0a77873a1b6d89f96f4c
Paul Ventura <christianpaulventura@gmail.com>
2020-04-15 11:34:48 +0800

    create shorten_url route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -19,6 +19,12 @@ def create_app():
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/api/v1/shorten_url', methods=['POST'])
+    def shorten_url():
+        original_url = request.form['original_url']
+
+        return original_url
 
     return app
 
