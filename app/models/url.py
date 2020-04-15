5fe54264db494d9810b3d3e1707bfe0063c2a3f7
Paul Ventura <christianpaulventura@gmail.com>
2020-04-15 13:54:47 +0800

    add size for varchar type

--- app/models/url.py
+++ app/models/url.py
@@ -13,8 +13,8 @@ class Url(db.Model):
     # Set the expiration for another 40 days
     expiration = today + datetime.timedelta(days=40)
 
-    hash = db.Column(db.String, primary_key=True)
-    original_url = db.Column(db.String, nullable=False)
+    hash = db.Column(db.String(16), primary_key=True)
+    original_url = db.Column(db.String(512), nullable=False)
     user_id = db.Column(db.Integer, index=True, nullable=True)
     creation_date = db.Column(db.DateTime, default=today)
     expiration_date = db.Column(db.DateTime, default=expiration)
--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,6 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, index=True, nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(32), unique=True, index=True, nullable=False)
     last_login = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
