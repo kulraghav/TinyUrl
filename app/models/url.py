2e95dabd18b904cd9076e6d2539f5ba1179fed05
Paul Ventura <christianpaulventura@gmail.com>
2020-04-15 14:16:42 +0800

    create url model

---
+++ app/models/url.py
@@ -0,0 +1,20 @@
+from app import db
+import datetime
+
+
+class Url(db.Model):
+    """
+    Model for url table
+    """
+
+    __tablename__ = "urls"
+
+    today = datetime.datetime.now()
+    # Set the expiration for another 40 days
+    expiration = today + datetime.timedelta(days=40)
+
+    hash = db.Column(db.String, primary_key=True)
+    original_url = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, index=True, nullable=True)
+    creation_date = db.Column(db.DateTime, default=today)
+    expiration_date = db.Column(db.DateTime, default=expiration)
